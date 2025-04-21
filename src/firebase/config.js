23ec601b2c6f539cd2111347e474a454f1c86ad3
Anir Achibane <anirf42@gmail.com>
2025-04-21 12:20:01 +0100

    guard secrets

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDf3IYfuDOOECYhhz4ZWtNaTrtAl0IEDA0",
-    authDomain: "devgate-6df87.firebaseapp.com",
-    projectId: "devgate-6df87",
-    storageBucket: "devgate-6df87.firebasestorage.app",
-    messagingSenderId: "233307387981",
+    apiKey: import.meta.env.APIKEY,
+    authDomain: import.meta.env.AUTHDOMAIN,
+    projectId: import.meta.env.PROJECTID,
+    storageBucket: import.meta.env.STORAGEBUCKET,
+    messagingSenderId: import.meta.env.MESSAGINGSENDERID,
     appId: "1:233307387981:web:0566380fda84ca6d8b8404",
-    measurementId: "G-NGKQQ1TF0D"
+    measurementId: import.meta.env.MEASUREMENTID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
