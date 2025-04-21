e5507802ab0104ffd88950a1a6832b782aee856f
SmollCoco <bentalebyoussef7799@gmail.com>
2025-04-21 12:49:56 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+// Import Bootstrap and its CSS
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+import { BootstrapVue3 } from "bootstrap-vue-3";
+
+// Import Font Awesome
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
+const app = createApp(App);
+
+// Use plugins
+app.use(router);
+app.use(BootstrapVue3);
+
+// Mount app
+app.mount("#app");
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="home">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is a simple Vue.js application.</p>
+        <router-link to="/about">Go to About Page</router-link>
+    </div>
+</template>
