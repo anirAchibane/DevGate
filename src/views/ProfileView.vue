28eb121b158a27bd4eb993ecae94919c36194726
SmollCoco <bentalebyoussef7799@gmail.com>
2025-04-27 00:55:35 +0100

    Navbar adjustments

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,13 +1,11 @@
 <template>
     <mini-navbar></mini-navbar>
-    <div>
-        
-    </div>
+    <div style="color: white">ProfileView.vue</div>
 </template>
 
 <script setup>
-import MiniNavbar from "@/components/MiniNavbar.vue"
-import { useRoute } from 'vue-router'
+import MiniNavbar from "@/components/MiniNavbar.vue";
+import { useRoute } from "vue-router";
 
 // User's ID from route params:
 const route = useRoute();
@@ -15,12 +13,13 @@ const userId = route.params.id;
 console.log(userId); // This will log the user ID from the URL
 </script>
 
-<style >
-html, body, #app {
+<style scoped>
+html,
+body,
+#app {
     height: 100%;
     margin: 0;
     padding: 0;
-    background-color: #0D1117;
+    background-color: #0d1117;
 }
-
 </style>
